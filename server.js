6ed45f0cffbf05b00914cd5d13fcd9393d1255d6
justinepeterson <jmahinyila@gmail.com>
2019-11-18 09:10:19 -0500

    Database fixes for herouku

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const passport = require("passport");
+const dotenv = require("dotenv");
 
 const users = require("./routes/api/users");
+
+dotenv.config();
 
 // Bodyparser middleware
 app.use(
@@ -14,14 +17,20 @@ app.use(
 );
 app.use(bodyParser.json());
 
-// DB Config
-const db = "mongodb://localhost:27017/kodemunit";
-
 // Connect to MongoDB
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://Justine:lim123@cluster0-stbf3.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 // Passport middleware
 app.use(passport.initialize());
@@ -30,8 +39,10 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-
-const port = process.env.PORT || 5000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
 
 // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
