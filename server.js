8e0678655eebac72d7b3fb4d2bb0fd57bc4e1826
justinepeterson <jmahinyila@gmail.com>
2019-11-22 04:37:02 -0500

    Killed the cors error 4 good

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ const home = require("./routes/api/index")
 dotenv.config();
 
 //CORS error 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
