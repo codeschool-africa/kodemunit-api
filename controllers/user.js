f6177c716ebb6fdace7ebbd6389aff26d0bbf7cb
justinepeterson <jmahinyila@gmail.com>
2019-11-30 07:37:29 -0500

    Created signup route

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,57 @@
-exports.signup = (req, res, next) => {};
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-exports.login = (req, res, next) => {};
+exports.signup = (req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+    });
+  });
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email }).then(
+    (user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error('User not found!')
+        });
+      }
+      bcrypt.compare(req.body.password, user.password).then(
+        (valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect password!')
+            });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: 'token'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+    }
+  ).catch(
+    (error) => {
+      res.status(500).json({
+        error: error
+      });
+    }
+  );
+}
