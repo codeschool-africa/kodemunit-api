8e7e36e234ba86b87e8d870eb654d47f2f122d9c
justinepeterson <jmahinyila@gmail.com>
2019-11-30 06:00:31 -0500

    Separated routes from the app

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
-const Student = require('./models/student')
+
+const studentRoutes = require('./routes/student')
 const app = express();
 
 dotenv.config();
@@ -25,103 +26,6 @@ app.use((req, res, next) => {
     next();
 });
 
-//sending student information to the database
-app.post('/api/student', (req, res, next) => {
-    const student = new Student({
-        first_name: req.body.first_name,
-        second_name: req.body.second_name,
-        date_of_birth: req.body.date_of_birth,
-        mobile: req.body.mobile,
-        email: req.body.email
-    });
-    student.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Student saved successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error.errmsg
-            });
-        }
-    );
-});
-
-//getting all the students details
-app.get('/api/student', (req, res, next) => {
-    Student.find().then(
-        (students) => {
-            res.status(200).json(students);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error.errmsg
-            });
-        }
-    );
-});
-
-//getting a single students details
-app.get('/api/student/:id', (req, res, next) => {
-    Student.findOne({
-        _id: req.params.id
-    }).then(
-        (student) => {
-            res.status(200).json(student);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error.errmsg
-            });
-        }
-    );
-});
-
-
-//Update student information 
-app.put('/api/student/:id', (req, res, next) => {
-    const student = new Student({
-        first_name: req.body.first_name,
-        second_name: req.body.second_name,
-        date_of_birth: req.body.date_of_birth,
-        mobile: req.body.mobile,
-        email: req.body.email,
-        studentId: req.body.studentId
-    });
-    Student.updateOne({ _id: req.params.id }, student).then(
-        () => {
-            res.status(201).json({
-                message: 'Updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error.errmsg
-            });
-        }
-    );
-});
-
-//Deleting one student
-app.delete('/api/student/:id', (req, res, next) => {
-    Student.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Student Deleted!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error.errmsg
-            });
-        }
-    );
-});
+app.use('/api/student', studentRoutes);
 
 module.exports = app;
--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
- 
-router.get('/', function (req, res) {
-    res.send('Welcome To Kodemunit-API')
-});
-
-module.exports = router;
