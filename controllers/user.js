da6be6d2275c3107f8f09404ded574fddab69004
justinepeterson <jmahinyila@gmail.com>
2019-12-01 09:36:50 -0500

    Resolved the error

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res) => {
   const newUser = new User({
@@ -14,44 +15,44 @@ exports.signup = (req, res) => {
       newUser
         .save()
         .then(user => res.json(user))
-        .catch(err => console.log(err));
+        .catch(user => res.json(user));
     });
   });
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email }).then(
-    (user) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
       if (!user) {
         return res.status(401).json({
-          error: new Error('User not found!')
+          error: {message:"user not found"}
         });
       }
-      bcrypt.compare(req.body.password, user.password).then(
-        (valid) => {
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then(valid => {
           if (!valid) {
             return res.status(401).json({
-              error: new Error('Incorrect password!')
+              error: {message:"Incorect password"}
             });
           }
+          const token = jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            expiresIn: "24h"
+          });
           res.status(200).json({
             userId: user._id,
-            token: 'token'
+            token: token
           });
-        }
-      ).catch(
-        (error) => {
+        })
+        .catch(error => {
           res.status(500).json({
             error: error
           });
-        }
-      );
-    }
-  ).catch(
-    (error) => {
+        });
+    })
+    .catch(error => {
       res.status(500).json({
         error: error
       });
-    }
-  );
-}
+    });
+};
